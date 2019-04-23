106bbd09a459ec52888fa1e7d1777aa774e3a662
josephone <49501259+josephone@users.noreply.github.com>
2019-04-23 15:00:43 +0100

    Check if team exists working

--- src/jk00687_project_com1028/LeagueDeveloperPage.java
+++ src/jk00687_project_com1028/LeagueDeveloperPage.java
@@ -5,7 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
@@ -54,7 +58,6 @@ public class LeagueDeveloperPage {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-
 		JLabel lblLeagueDeveloperView = new JLabel("League developer view");
 		lblLeagueDeveloperView.setBounds(10, 11, 134, 14);
 		frame.getContentPane().add(lblLeagueDeveloperView);
@@ -94,22 +97,28 @@ public class LeagueDeveloperPage {
 		JLabel lblResult = new JLabel("Result:");
 		lblResult.setBounds(10, 160, 46, 14);
 		frame.getContentPane().add(lblResult);
-		
+
 		JButton updateTableBtn = new JButton("Update tables");
 		updateTableBtn.setBounds(10, 215, 114, 23);
 		frame.getContentPane().add(updateTableBtn);
-		
+
 		updateTableBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String teamNameString = teamName.getText();
 				String matchResultString = matchResult.getText();
 				String goalsScoredString = goalsScored.getText();
 				String goalsConcededString = goalsConceded.getText();
-				updateTable(teamNameString, matchResultString, goalsScoredString, goalsConcededString);
-				JOptionPane.showMessageDialog(null, "Tables successfully updated");
+				try {
+					updateTable(teamNameString, matchResultString, goalsScoredString, goalsConcededString);
+
+					JOptionPane.showMessageDialog(null, "Tables successfully updated");
+				} catch (IllegalArgumentException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					JOptionPane.showMessageDialog(null, "Tables could not be updated");
+				}
 			}
 		});
-		
 
 		JButton editStatsBtn = new JButton("Edit statistics");
 		editStatsBtn.setBounds(262, 215, 134, 23);
@@ -121,64 +130,103 @@ public class LeagueDeveloperPage {
 				EditStats.main();
 				frame.dispose();
 			}
-			
+
 		});
 
 	}
 
-	public void updateTable(String teamNameFinal, String matchResultFinal, String goalsScoredFinal, String goalsConcededFinal)
-			throws IllegalArgumentException {
-		
-		// TODO: INCREMENT GAMES PLAYED, MOVE TEAMS HIGHER OR LOWER DEPENDING ON THEIR POINTS
+	public void updateTable(String teamNameFinal, String matchResultFinal, String goalsScoredFinal,
+			String goalsConcededFinal) throws IllegalArgumentException, IOException {
+
+		// TODO: INCREMENT GAMES PLAYED, MOVE TEAMS HIGHER OR LOWER DEPENDING ON THEIR
+		// POINTS
 
 		String file_name = "C:/Users/hunya/Documents/GitHub/COM1033_Assignment1/league_table_test.txt";
 		String RegExPattern = "[a-zA-Z]+[\\t]{2}[0-9]+";
-		try {
-			FileWriter write = new FileWriter(file_name, true);
-			BufferedWriter writeBuffer = new BufferedWriter(write);
-			String newLine = System.getProperty("line.separator");
-			writeBuffer.write(newLine);
-			writeBuffer.write(teamNameFinal + "\t" + "\t");
-			writeBuffer.write("1" + "\t" + "\t");
-			if (matchResultFinal.equals("Win")) {
-				writeBuffer.write("1" + "\t");
-				writeBuffer.write("0" + "\t");
-				writeBuffer.write("0" + "\t");
-			} else if (matchResultFinal.equals("Draw")) {
-				writeBuffer.write("0" + "\t");
-				writeBuffer.write("1" + "\t");
-				writeBuffer.write("0" + "\t");
-			} else if (matchResultFinal.equals("Loss")) {
-				writeBuffer.write("0" + "\t");
-				writeBuffer.write("0" + "\t");
-				writeBuffer.write("1" + "\t");
-			} else {
-				throw new IllegalArgumentException("Result must be a 'Win', 'Draw' or 'Loss'");
+		
+		FileWriter write = new FileWriter(file_name, true);
+		BufferedWriter writeBuffer = new BufferedWriter(write);
+		String newLine = System.getProperty("line.separator");
+		
+		if (checkTeamExists(teamNameFinal)) {
+			
+			System.out.println("Team already exists");
+
+		} else {
+
+			try {
+
+				writeBuffer.write(newLine);
+				writeBuffer.write(teamNameFinal + "\t" + "\t");
+				writeBuffer.write("1" + "\t" + "\t");
+				if (matchResultFinal.equals("Win")) {
+					writeBuffer.write("1" + "\t");
+					writeBuffer.write("0" + "\t");
+					writeBuffer.write("0" + "\t");
+				} else if (matchResultFinal.equals("Draw")) {
+					writeBuffer.write("0" + "\t");
+					writeBuffer.write("1" + "\t");
+					writeBuffer.write("0" + "\t");
+				} else if (matchResultFinal.equals("Loss")) {
+					writeBuffer.write("0" + "\t");
+					writeBuffer.write("0" + "\t");
+					writeBuffer.write("1" + "\t");
+				} else {
+					throw new IllegalArgumentException("Result must be a 'Win', 'Draw' or 'Loss'");
+				}
+
+				writeBuffer.write(goalsScoredFinal + "\t" + "\t");
+				writeBuffer.write(goalsConcededFinal + "\t" + "\t");
+
+				int goalSc = Integer.valueOf(goalsScoredFinal);
+				int goalCon = Integer.valueOf(goalsConcededFinal);
+				int goalDifference = goalSc - goalCon;
+
+				writeBuffer.write(goalDifference + "\t" + "\t");
+
+				if (matchResultFinal.equals("Win")) {
+					writeBuffer.write("3");
+				} else if (matchResultFinal.equals("Draw")) {
+					writeBuffer.write("1");
+				} else if (matchResultFinal.equals("Loss")) {
+					writeBuffer.write("0");
+				} else {
+					throw new IllegalArgumentException();
+				}
+				writeBuffer.close();
+			} catch (IOException e) {
+
 			}
-
-			writeBuffer.write(goalsScoredFinal + "\t" + "\t");
-			writeBuffer.write(goalsConcededFinal + "\t" + "\t");
-
-			int goalSc = Integer.valueOf(goalsScoredFinal);
-			int goalCon = Integer.valueOf(goalsConcededFinal);
-			int goalDifference = goalSc - goalCon;
-
-			writeBuffer.write(goalDifference + "\t" + "\t");
-
-			if (matchResultFinal.equals("Win")) {
-				writeBuffer.write("3");
-			} else if (matchResultFinal.equals("Draw")) {
-				writeBuffer.write("1");
-			} else if (matchResultFinal.equals("Loss")) {
-				writeBuffer.write("0");
-			} else {
-				throw new IllegalArgumentException();
-			}
-			writeBuffer.close();
-		} catch (IOException e) {
-
 		}
-
 	}
 
+	public boolean checkTeamExists(String teamName) throws IOException {
+		File f1 = new File("C:/Users/hunya/Documents/GitHub/COM1033_Assignment1/league_table_test.txt"); // Creation of
+																											// File
+																											// Descriptor
+																											// for input
+																											// file
+		String[] words = null; // Intialize the word Array
+		FileReader fr = new FileReader(f1); // Creation of File Reader object
+		BufferedReader br = new BufferedReader(fr); // Creation of BufferedReader object
+		String s;
+		String input = teamName; // Input word to be searched
+		int count = 0; // Intialize the word to zero
+		while ((s = br.readLine()) != null) // Reading Content from the file
+		{
+			words = s.split("\t"); // Split the word using space
+			for (String word : words) {
+				if (word.equals(input)) // Search for the given word
+				{
+					count++; // If Present increase the count by one
+				}
+			}
+		}
+		if (count != 0) // Check for count not equal to zero
+		{
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- src/jk00687_project_com1028/PlayerPage.java
+++ src/jk00687_project_com1028/PlayerPage.java
@@ -9,6 +9,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,10 +24,12 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.text.JTextComponent;
+import javax.swing.JTextField;
 
 public class PlayerPage {
 
 	private JFrame frame;
+	private JTextField teamName;
 
 	/**
 	 * Launch the application.
@@ -48,94 +51,132 @@ public class PlayerPage {
 	 * Create the application.
 	 */
 	public PlayerPage() {
-		initialize();
+		try {
+			initialize();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws IOException 
 	 */
-	private void initialize() {
+	private void initialize() throws IOException {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 320, 302);
+		frame.setBounds(100, 100, 483, 336);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		JLabel lblPlayerView = new JLabel("Player view");
 		lblPlayerView.setBounds(10, 11, 96, 14);
 		frame.getContentPane().add(lblPlayerView);
+		
+		teamName = new JTextField();
+		teamName.setBounds(242, 102, 86, 20);
+		frame.getContentPane().add(teamName);
+		teamName.setColumns(10);
+		
+		String teamNameString = teamName.getText();
 
 		try {
-			viewTeamStatistics();
+			viewTeamStatistics(teamNameString);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 
 		JLabel lblTeamStatistics = new JLabel("Team statistics");
-		lblTeamStatistics.setBounds(10, 83, 96, 14);
+		lblTeamStatistics.setBounds(10, 155, 96, 14);
 		frame.getContentPane().add(lblTeamStatistics);
-	}
-	
-	public boolean findTeam(String teamName) throws FileNotFoundException {
 		
-		String content = new Scanner(new File("C:/Users/hunya/Documents/GitHub/COM1033_Assignment1/team_statistics.txt")).useDelimiter("\\Z").next();
-		String pattern = teamName;
+		JLabel lblEnterTheName = new JLabel("Enter the name of the team you wish to view:");
+		lblEnterTheName.setBounds(10, 91, 256, 42);
+		frame.getContentPane().add(lblEnterTheName);
+		
+		
+		
+	}
 
-		Pattern patternString = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
-		Matcher matcher = patternString.matcher(content);
+	public void viewTeamStatistics(String teamNameString) throws IOException {
+		
+		JTextArea teamStatistics = new JTextArea();
+		teamStatistics.setFont(new Font("Monospaced", Font.PLAIN, 10));
+		teamStatistics.setBounds(10, 180, 340, 70);
+		frame.getContentPane().add(teamStatistics);
+		
+		if(checkTeamExists(teamNameString)) {
+			String file_name = "C:/Users/hunya/Documents/GitHub/COM1033_Assignment1/team_statistics.txt";
+			Scanner input = new Scanner(new File(file_name));
+			int count = 0;
+			
+			while (input.hasNext() && count <= 15) {
+				int tracker = 0;
+				count++;
+				String word = input.next();
 
-		if (matcher.find()) {
+				if (count == 13 && tracker == 0) {
+					tracker++;
+					try {
+						
+						teamStatistics.setText("Total goals scored: " + word + "\n");
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}if (count == 14 && tracker == 1) {
+					tracker++;
+					String firstString = teamStatistics.getText();
+					try {
+						
+						teamStatistics.setText(firstString + "Total goals conceded: " + word + "\n" + "Overall goal difference: ");
+						
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else {
+					try {
+						String secondString = teamStatistics.getText();
+						
+						
+						teamStatistics.setText(secondString + "Overall goal difference: " + word);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}else {
+			throw new IllegalArgumentException("Team does not exist");
+		}
+
+	}
+
+	public boolean checkTeamExists(String teamName) throws IOException {
+		File f1 = new File("C:/Users/hunya/Documents/GitHub/COM1033_Assignment1/league_table_test.txt"); // Creation of
+																											// File
+																											// Descriptor
+																											// for input
+																											// file
+		String[] words = null; // Intialize the word Array
+		FileReader fr = new FileReader(f1); // Creation of File Reader object
+		BufferedReader br = new BufferedReader(fr); // Creation of BufferedReader object
+		String s;
+		String input = teamName; // Input word to be searched
+		int count = 0; // Intialize the word to zero
+		while ((s = br.readLine()) != null) // Reading Content from the file
+		{
+			words = s.split("\t"); // Split the word using space
+			for (String word : words) {
+				if (word.equals(input)) // Search for the given word
+				{
+					count++; // If Present increase the count by one
+				}
+			}
+		}
+		if (count != 0) // Check for count not equal to zero
+		{
 			return true;
 		} else {
 			return false;
 		}
 	}
-
-
-	public void viewTeamStatistics() throws FileNotFoundException {
-		JTextArea teamStatistics = new JTextArea();
-		teamStatistics.setFont(new Font("Monospaced", Font.PLAIN, 10));
-		teamStatistics.setBounds(10, 108, 196, 142);
-		frame.getContentPane().add(teamStatistics);
-		
-		String file_name = "C:/Users/hunya/Documents/GitHub/COM1033_Assignment1/team_statistics.txt";
-		Scanner input = new Scanner(new File(file_name));
-		int count = 0;
-		
-		while (input.hasNext() && count <= 15) {
-			int tracker = 0;
-			count++;
-			String word = input.next();
-
-			if (count == 13 && tracker == 0) {
-				tracker++;
-				try {
-					
-					teamStatistics.setText("Total goals scored: " + word + "\n");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}if (count == 14 && tracker == 1) {
-				tracker++;
-				String firstString = teamStatistics.getText();
-				try {
-					
-					teamStatistics.setText(firstString + "Total goals conceded: " + word + "\n" + "Overall goal difference: ");
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			} else {
-				try {
-					String secondString = teamStatistics.getText();
-					
-					
-					teamStatistics.setText(secondString + "Overall goal difference: " + word);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-	}
-
 }
